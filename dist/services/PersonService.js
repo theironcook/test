932b733dfd57bcd85a14f3a21b803cdfc3a66b71
Bart Wood <bartwood@gmail.com>
2019-10-11 15:50:26 -0600

    test

--- dist/services/PersonService.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class PersonService {
-}
-exports.PersonService = PersonService;
-exports.personService = new PersonService();
